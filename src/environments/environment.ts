087a84e26451fd78748a9907e3e8d1030daee912
Rosen Koychev <rosen.koichev@gmail.com>
2023-07-25 10:31:41 +0300

    Installing Firebase+Host

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'eshop-project-9ac49',
+    appId: '1:864652752522:web:3bdd986c09e391ddb45f73',
+    storageBucket: 'eshop-project-9ac49.appspot.com',
+    apiKey: 'AIzaSyBdxoo7fg5-4lwnBbHc0cnBms1DBYnDl3Y',
+    authDomain: 'eshop-project-9ac49.firebaseapp.com',
+    messagingSenderId: '864652752522',
+  },
+};
