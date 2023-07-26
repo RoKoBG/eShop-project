ba4e9d6a7df43137fa677cea1824938d428a351f
Rosen Koychev <rosen.koichev@gmail.com>
2023-07-26 19:59:09 +0300

    Started to implement Profile page

--- src/app/auth/profile/profile.component.ts
+++ src/app/auth/profile/profile.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { onAuthStateChanged } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  authh = getAuth();
 
+  constructor(private router: Router, public auth: AngularFireAuth) {}
+
+  ngOnInit(): void {
+    onAuthStateChanged(this.authh, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        console.log(user.uid);
+        console.log(user.email);
+        // ...
+      } else {
+        return;
+      }
+    });
+  }
 }
